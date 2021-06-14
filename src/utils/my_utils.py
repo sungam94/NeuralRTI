f35e5d9540dbaa1fa828ac48bffb4e163e34a964
Magnus Kaffenberger <magnus.kaffenberger@gmail.com>
2021-06-14 20:50:55 +0200

    Update my_utils.py

--- src/utils/my_utils.py
+++ src/utils/my_utils.py
@@ -4,7 +4,7 @@ import colour
 import matplotlib.pyplot as plt
 import torch
 import torchvision as tv
-from ranger import Ranger
+import Ranger
 # optimizer
 from torch.optim import SGD, Adam
 # scheduler
